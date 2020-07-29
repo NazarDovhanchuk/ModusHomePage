ed09dc0801b95571bcb6285dbe89ab281eb3d6a4
NazarDovhanchuk <nazardovhanchuk@gmail.com>
2020-07-29 10:50:39 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// 'use strict';
+
+module.exports = {
+  extends: '@mate-academy/eslint-config',
+  parserOptions: {
+    sourceType: 'module',
+    env: {
+      jest: true,
+    },
+    plugins: ['jest'],
+  },
+
+};
