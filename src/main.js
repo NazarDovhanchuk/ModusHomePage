44fe18e46dcf51dda508cf62437c2f4b88aeb55e
Nazar <nazardovhanchuk@gmail.com>
2020-08-02 08:32:10 +0300

    add gropdown menu

--- src/main.js
+++ src/main.js
@@ -1,9 +1,44 @@
-$(document).ready(function(){
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+$('document').ready(function(){
   $('.slider').slick({
     dots: true
   });
 })
 
+
+const mySwiper = new Swiper ('.swiper-container', {
+	nextButton: '.swiper-right',
+	prevButton: '.swiper-left',
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 6,
+	autoplay: 10000,
+	spaceBetween: 30,
+	breakpoints: {
+
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 10
+		},
+
+		480: {
+			slidesPerView: 2,
+			spaceBetween: 20
+		},
+
+		768: {
+			slidesPerView: 3,
+			spaceBetween: 30
+		},
+
+		992: {
+			slidesPerView: 4,
+			spaceBetween: 30
+		}
+	}
+});
 
 
 
